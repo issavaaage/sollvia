2b7452cd564685294a706c27736a1420d3ca9180
bartolemio <bartolemio@gmail.com>
2020-10-04 23:21:48 +0300

    Updates

---
+++ precache-manifest.d1eda3ae9fc2a4e7059876e937edb6a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662d53c8da44450492d7e186181e3021",
+    "url": "/sollvia/index.html"
+  },
+  {
+    "revision": "5030bb5f0b785180a397",
+    "url": "/sollvia/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "c350ccd8c1001f7fbe92",
+    "url": "/sollvia/static/css/main.b2faf055.chunk.css"
+  },
+  {
+    "revision": "5030bb5f0b785180a397",
+    "url": "/sollvia/static/js/2.f6b8a934.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sollvia/static/js/2.f6b8a934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c350ccd8c1001f7fbe92",
+    "url": "/sollvia/static/js/main.25eb492d.chunk.js"
+  },
+  {
+    "revision": "78d1524e18caf958e64b",
+    "url": "/sollvia/static/js/runtime-main.3ba40e18.js"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/sollvia/static/media/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/sollvia/static/media/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/sollvia/static/media/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "1d0b4f9853ebf396cf43cff31a833381",
+    "url": "/sollvia/static/media/background_home.1d0b4f98.png"
+  },
+  {
+    "revision": "eacc4b2bf49cd580390f20210c468d11",
+    "url": "/sollvia/static/media/bob.eacc4b2b.png"
+  },
+  {
+    "revision": "5dae9692c13a2d45ea9412e1ae762a38",
+    "url": "/sollvia/static/media/logo.5dae9692.svg"
+  }
+]);
